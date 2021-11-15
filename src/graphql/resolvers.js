f7b1df91057de9a918513c02b9a7680cf2f089a7
José David Fontalvo <90539362+FontalvoJ@users.noreply.github.com>
2021-11-14 22:15:28 -0500

    Add insciptions

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const userCtrl = require('../controllers/user');
 const projectCtrl = require('../controllers/project');
+const inscriptionCtrl = require('../controllers/inscriptions')
 
 
 // Resolvers
@@ -11,17 +12,26 @@ const resolvers = {
     // Project
     getProjects: async () => projectCtrl.getProjects(),
     getProject: async (_, { id }) => projectCtrl.getProject(id),
+    // Project
+    getInscriptions: async () => inscriptionCtrl.getInscriptions(),
+    getInscription: async (_, { id }) => inscriptionCtrl.getInscription(id),
+
   },
   Mutation: {
     // User
     newUser: async (_, { input }) => userCtrl.newUser(input),
     authUser: async (_, { input }) => userCtrl.authUser(input),
-    updateUser: async (_, {id, input}) => userCtrl.updateUser(id, input),
-    deleteUser: async (_, {id}) => userCtrl.deleteUser(id),
+    updateUser: async (_, { id, input }) => userCtrl.updateUser(id, input),
+    deleteUser: async (_, { id }) => userCtrl.deleteUser(id),
     // Project
     newProject: async (_, { input }) => projectCtrl.getProject(input),
     updateProject: async (_, { id, input }) => projectCtrl.updateProject(id, input),
-    deleteProject: async (_, {id}) => projectCtrl.deleteProject(id),
+    deleteProject: async (_, { id }) => projectCtrl.deleteProject(id),
+
+    // Incription
+    newInscription: async (_, { input }) => inscriptionCtrl.getInscription(input),
+    updateInscription: async (_, { id, input }) => inscriptionCtrl.updateInscription(id, input),
+    deleteInscription: async (_, { id }) => inscriptionCtrl.deleteInscription(id),
   },
 };
 
