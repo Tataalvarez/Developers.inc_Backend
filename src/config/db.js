597537d665fb1c36f360dba2f78b0b66769b826a
Tataalvarez <tatianaalvarezmartinez@gmail.com>
2021-12-18 01:47:48 -0500

    heroku

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const PORT = process.variables.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config({path: 'variables.env'});
 
 const conectarDB = async () => {
